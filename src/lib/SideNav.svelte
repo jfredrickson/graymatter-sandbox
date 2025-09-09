c10f004f98fd737077ac074ffd8dc66bf5087c5c
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2025-09-09 14:45:20 -0600

    initial import

---
+++ src/lib/SideNav.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import "@gsa-tts/graymatter-ui/styles/base/global.css";
+
+  import DesktopSideNav from "@gsa-tts/graymatter-ui/components/DesktopSideNav.svelte";
+  import LogoutIcon from "@gsa-tts/graymatter-ui/assets/images/ai-icons/main-nav/logout.svg";
+
+  let { children } = $props();
+
+  const apiUrl = "#";
+  const chatUrl = "#";
+  const consoleUrl = "#";
+  const discoverUrl = "#";
+
+  const profileMenuData = {
+    userMeta: {
+      initials: "HI",
+    },
+    menuItems: [
+      {
+        label: "Log out",
+        icon: LogoutIcon,
+        action: "logout",
+      },
+    ],
+  };
+</script>
+
+<DesktopSideNav {apiUrl} {chatUrl} {consoleUrl} {discoverUrl} {profileMenuData}>
+  {@render children?.()}
+</DesktopSideNav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import favicon from "$lib/assets/favicon.svg";
+  import SideNav from "$lib/SideNav.svelte";
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
+
+<SideNav>
+  <dev>Side nav content</dev>
+</SideNav>
+
+{@render children?.()}
